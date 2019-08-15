0583bbd9128fbe87aa8dd9c0ff586d1e777e3d84
toliner <toliner.rkt@gmail.com>
2019-08-16 03:52:59 +0900

    move configurations to root build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -7,14 +8,26 @@ plugins {
 group = "jp.justincase"
 version = "0.0.1"
 
-repositories {
-    mavenCentral()
-}
+subprojects {
+    apply<KotlinPluginWrapper>()
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
+    group = "jp.justincase"
+    version = "0.0.1"
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+
+        implementation("com.squareup:kotlinpoet:1.3.0")
+        // AWS CDK
+        implementation("software.amazon.awscdk", "lambda", "1.3.0.DEVPREVIEW")
+        implementation("software.amazon.awscdk", "logs-destinations", "1.3.0.DEVPREVIEW")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
