9c7a1aad4ee8b6c57ea2187306b4af9c2113eab1
toliner <toliner.rkt@gmail.com>
2019-08-23 11:21:58 +0900

    add maven-publish to generator

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,7 +1,20 @@
 plugins {
     id("application")
+    id("maven-publish")
 }
 
 application {
     mainClassName = "jp.justincase.cdkdsl.generator.MainKt"
+}
+
+publishing {
+    publications {
+        register("maven", MavenPublication::class.java) {
+            groupId = "jp.justincase"
+            artifactId = "cdk-dsl-generator"
+            version = project.version as String
+
+            from(project.components["java"])
+        }
+    }
 }
