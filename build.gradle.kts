a0b952d8d7866d9f73ffa026a2af559adf90be9e
toliner <toliner.rkt@gmail.com>
2019-08-26 13:25:02 +0900

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
     id("maven-publish")
     id("com.jfrog.bintray") version "1.8.4"
 }
