a46ac2d6632194ed33e8e3932f22ba4a0a71d894
toliner <toliner.rkt@gmail.com>
2019-08-28 19:13:00 +0900

    make getModuleDependencies suspend

--- buildSrc/src/main/kotlin/DependencyHandler.kt
+++ buildSrc/src/main/kotlin/DependencyHandler.kt
@@ -7,7 +7,6 @@ import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.io.jvm.javaio.toInputStream
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -18,29 +17,30 @@ lateinit var moduleDependencyMap: Map<String, List<String>>
     private set
 
 @KtorExperimentalAPI
-fun getModuleDependencies(): Map<String, List<String>> = runBlocking {
-    if (::moduleDependencyMap.isInitialized) return@runBlocking moduleDependencyMap
-    cdkLatestVersions.filterValues { it.isNotEmpty() }.keys.associateWith { module ->
-        async {
-            val version = cdkLatestVersions.getValue(module).last().toString()
-            val targetUrl =
-                "http://central.maven.org/maven2/software/amazon/awscdk/$module/${version}/$module-${
-                version}.pom"
-            val doc = withContext(Dispatchers.IO) {
-                val response = client.get<HttpResponse>(targetUrl)
+suspend fun getModuleDependencies(): Map<String, List<String>> {
+    return if (::moduleDependencyMap.isInitialized) moduleDependencyMap else withContext(Dispatchers.Default) {
+        cdkLatestVersions.filterValues { it.isNotEmpty() }.keys.associateWith { module ->
+            async {
+                val version = cdkLatestVersions.getValue(module).last().toString()
+                val targetUrl =
+                    "http://central.maven.org/maven2/software/amazon/awscdk/$module/${version}/$module-${
+                    version}.pom"
+                val doc = withContext(Dispatchers.IO) {
+                    val response = client.get<HttpResponse>(targetUrl)
 
-                check(response.status == HttpStatusCode.OK) { "${response.status} on accessing $targetUrl" }
-                DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(response.content.toInputStream())
+                    check(response.status == HttpStatusCode.OK) { "${response.status} on accessing $targetUrl" }
+                    DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(response.content.toInputStream())
+                }
+                doc.getElementsByTagName("dependency").asList().map { node ->
+                    PomArtifact(
+                        node.childNodes.asList().single { it.nodeName == "groupId" }.textContent,
+                        node.childNodes.asList().single { it.nodeName == "artifactId" }.textContent,
+                        node.childNodes.asList().single { it.nodeName == "version" }.textContent
+                    )
+                }.filter { it.groupId == "software.amazon.awscdk" }.map { it.artifactId }
             }
-            doc.getElementsByTagName("dependency").asList().map { node ->
-                PomArtifact(
-                    node.childNodes.asList().single { it.nodeName == "groupId" }.textContent,
-                    node.childNodes.asList().single { it.nodeName == "artifactId" }.textContent,
-                    node.childNodes.asList().single { it.nodeName == "version" }.textContent
-                )
-            }.filter { it.groupId == "software.amazon.awscdk" }.map { it.artifactId }
-        }
-    }.mapValues { it.value.await() }.apply { moduleDependencyMap = this }
+        }.mapValues { it.value.await() }.apply { moduleDependencyMap = this }
+    }
 }
 
 data class PomArtifact(
