372691b0dcb884ef46a721f8476e415d908de73a
toliner <toliner.rkt@gmail.com>
2019-09-06 13:33:22 +0900

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val awsCdkVersion: String by project
 
 allprojects {
     group = "jp.justincase"
-    version = "$awsCdkVersion-0.4.3"
+    version = "$awsCdkVersion-0.5.0"
 
     repositories {
         mavenCentral()
