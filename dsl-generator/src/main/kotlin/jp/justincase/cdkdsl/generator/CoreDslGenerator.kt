7f30571953096b2185e36b75c0dafff8dab24eb4
toliner <toliner.rkt@gmail.com>
2019-11-18 16:57:07 +0900

    create GenerationTarget and add it to parameter of generators

--- dsl-generator/src/main/kotlin/jp/justincase/cdkdsl/generator/CoreDslGenerator.kt
+++ dsl-generator/src/main/kotlin/jp/justincase/cdkdsl/generator/CoreDslGenerator.kt
@@ -1,15 +1,18 @@
 package jp.justincase.cdkdsl.generator
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.mapNotNull
 import software.amazon.awscdk.core.Construct
 import java.io.File
 import java.lang.reflect.Modifier
 
 object CoreDslGenerator : ICdkDslGenerator {
+
     override suspend fun run(classes: Flow<Class<out Any>>, targetDir: File, moduleName: String, packageName: String) {
         val filtered = classes.filter { clazz ->
-            if (clazz.isInterface) {
+            if (clazz.modifiers and (Modifier.ABSTRACT or Modifier.INTERFACE) != 0) {
                 /*
                 フィルター条件(インターフェースの場合):
                 1. Builderクラスを持っていること
@@ -28,14 +31,45 @@ object CoreDslGenerator : ICdkDslGenerator {
                 */
                 clazz.declaredClasses.singleOrNull { it.name == "Builder" }?.let { builder ->
                     builder.declaredMethods.any {
-                        it.name == "create" && it.parameterTypes.let { types ->
-                            types[0] == Construct::class.java && types[1] == String::class.java
-                        }
+                        it.name == "create" &&
+                            it.parameterCount == 0 || (
+                            it.parameterCount == 2 &&
+                                it.parameterTypes[0] == Construct::class.java &&
+                                it.parameterTypes[1] == java.lang.String::class.java
+                            )
                     }
                 } == true
             }
         }
-        ConstructorFunctionGenerator.run(filtered, targetDir, packageName)
-        PropClassExtensionGenerator.run(filtered, targetDir, packageName)
+        filtered.mapNotNull { clazz ->
+            val builder = clazz.declaredClasses.single { it.name == "Builder" }
+            clazz to when {
+                (clazz.modifiers and (Modifier.ABSTRACT or Modifier.INTERFACE)) != 0 -> GenerationTarget.INTERFACE
+                builder.isResourceTypeBuilder() -> GenerationTarget.RESOURCE
+                builder.isPropertyOnlyTypeBuilder() -> GenerationTarget.NO_ID
+                else -> return@mapNotNull null
+            }
+        }.collect {
+            // ToDo
+        }
+    }
+
+    private fun Class<*>.isResourceTypeBuilder() =
+        declaredMethods.single { it.name == "create" }.let {
+            it.parameterCount == 2 &&
+                it.parameterTypes[0] == Construct::class.java &&
+                it.parameterTypes[1] == java.lang.String::class.java
+        }
+
+    private fun Class<*>.isPropertyOnlyTypeBuilder() =
+        declaredMethods.single { it.name == "create" }.parameterCount == 0
+
+    enum class GenerationTarget {
+        // Interface, abstract class
+        INTERFACE,
+        // scope: Construct, id: Stringをcreate()の引数に持つもの
+        RESOURCE,
+        // create()の引数が空のもの
+        NO_ID
     }
 }
